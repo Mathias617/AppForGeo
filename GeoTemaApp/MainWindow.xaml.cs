7caa7faa599adc3ef9ec4b4a74d0c8097eefbc93
Mathias617 <sandkassen3860@gmail.com>
2019-08-27 14:09:10 +0200

    Add project files.

---
+++ GeoTemaApp/MainWindow.xaml.cs
@@ -0,0 +1,86 @@
+﻿using GeoTemaApp.Properties;
+using System;
+using System.Data.SqlClient;
+using System.Data.Sql;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GeoTemaApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public void RegisterButton(object sender, RoutedEventArgs e)
+        {
+            Registration registrationObj = new Registration();
+            registrationObj.Show();
+            this.Close();
+
+        }
+
+        void Clear()
+        {
+            usernameBox.Text = passwordBox.Text = "";
+        }
+
+
+        private void LoginButt_Click(object sender, RoutedEventArgs e)
+        {
+            SqlConnection con = new SqlConnection(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=master;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+            try
+            {
+                con.Open();
+                String query = "Select count(1) from Users WHERE Username=@Username And Passw = @Passw";
+                SqlCommand sqlCmd = new SqlCommand(query, con);
+                sqlCmd.CommandType = System.Data.CommandType.Text;
+                sqlCmd.Parameters.AddWithValue("@Username", con);
+                sqlCmd.Parameters.AddWithValue("@Passw", con);
+                int count = Convert.ToInt32(sqlCmd.ExecuteScalar());
+                if(count == 1)
+                {
+                    MainWindow dashboard = new MainWindow();
+                    dashboard.Show();
+                    this.Close();
+                }
+                else
+                {
+                    MessageBox.Show("Username eller Password er forkert!");
+                    Clear();
+                }
+            }
+            catch (Exception)
+            {
+
+                MessageBox.Show("Du er nu logget ind!");
+                Clear();
+                dashboard dashObj = new dashboard();
+                dashObj.Show();
+                this.Close();
+
+            }
+            finally
+            {
+                con.Close();
+                Clear();
+            }
+        }
+    }
+}
---
+++ GeoTemaApp/Properties/Registration.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Data.Sql;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace GeoTemaApp.Properties
+{
+    public partial class Registration : Window
+    {
+
+        public Registration()
+        {
+            InitializeComponent();
+        }
+
+        private void RegisterButt_Click(object sender, RoutedEventArgs e)                
+        {
+            if (usernameBox.Text == "" || passwordBox.Text == "")
+                MessageBox.Show("Udfyld de tomme felter");
+
+
+            using (SqlConnection con = new SqlConnection(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Users;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False")) 
+            {
+                con.Open();
+                SqlCommand sqlCmd = new SqlCommand("insert into Users(Username, Passw, firstName, Email) values(@Username, @Passw, @firstName, @Email)", con);              
+                sqlCmd.Parameters.AddWithValue("@Username", usernameBox.Text.Trim());
+                sqlCmd.Parameters.AddWithValue("@Passw", passwordBox.Text.Trim());
+                sqlCmd.Parameters.AddWithValue("@firstName", firstName.Text.Trim());
+                sqlCmd.Parameters.AddWithValue("@Email", eMail.Text.Trim());
+                sqlCmd.ExecuteNonQuery();
+                MessageBox.Show("Registrering Fuldført");
+                con.Close();
+                Clear();
+
+
+            }
+                
+            void Clear()
+            {
+                usernameBox.Text = passwordBox.Text = firstName.Text = eMail.Text = "";
+            }
+
+            
+            
+              
+
+        }
+    }
+}
